8731f9fecdea9889bf4a6c9c692a6a673c78ba4f
Prakash Sejwani <prakashsejwani@gmail.com>
2026-02-06 18:26:44 +0530

    feat(HolyGrail): implement component

---
+++ src/components/HolyGrail/HolyGrail.tsx
@@ -0,0 +1,15 @@
+import './HolyGrail.scss';
+
+export default function HolyGrail() {
+  return (
+    <div className="holy-grail-layout">
+      <header>Header</header>
+      <div className="layout-body">
+        <nav>Navigation</nav>
+        <main>Main Content</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </div>
+  );
+}
